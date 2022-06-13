b0484b39e064b711a52113f292feb0afabd2085b
Dariusz Kacban <darek.kacban@gmail.com>
2022-06-13 21:53:27 +0200

    Update Model.cs

--- DecisionEngine.Domain/Model.cs
+++ DecisionEngine.Domain/Model.cs
@@ -6,13 +6,11 @@ using System.Threading.Tasks;
 
 namespace DecisionEngine.Domain
 {
-    public class Model
+    public class BikeModel
     {
         public string Manufacturer  { get; set; }
         public string ModelName { get; set; }
         public decimal Price { get; set; }
         public string OfferUrl { get; set; }
-
-
     }
 }
